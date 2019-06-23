64da9fed8fdd4671aa8b1a66993d3825554842b4
unknown <fdhcg@live.com>
2019-06-23 10:34:27 +0800

    v0.81

--- wbdweb/settings.py
+++ wbdweb/settings.py
@@ -110,8 +110,12 @@ WSGI_APPLICATION = 'wbdweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'wbd',
+        'USER': 'pcnss',
+        'PASSWORD': '123456',
+        'HOST':'192.168.1.114',
+        'PORT':'3306',
     }
 }
 
